ac332cf9e0c86e4c9eba7dd287064bad69b0636e
Jakub Biela <jak.biela000@gmail.com>
2023-09-08 07:15:28 +0200

    Draw data points

--- Grapher.php
+++ Grapher.php
@@ -4,22 +4,22 @@ require_once 'GrapherDB.php';
 
 class Grapher
 {
+    const PADDING_X = 70;
+    const PADDING_Y = 30;
+    const LABEL_FONT_SIZE = 5;
+    const VALUE_INDICATOR_SIZE = 5;
+    const DOT_SIZE = 5;
+    const Y_AXIS_LABEL_COUNT = 6;
     private $image_width;
     private $image_height;
     private $image;
     private $colors = array();
-    private $PADDING_X = 70;
-    private $PADDING_Y = 30;
     private $safe_space = array();
     private $safe_image_width;
     private $safe_image_height;
     private $database = null;
     private $labels = array();
     private $data = array();
-    const LABEL_FONT_SIZE = 5;
-    const VALUE_INDICATOR_SIZE = 5;
-    const DOT_SIZE = 5;
-    const Y_AXIS_LABEL_COUNT = 6;
 
     public function __construct($image_width, $image_height)
     {
@@ -39,15 +39,17 @@ class Grapher
         $this->fetchgraphdata();
 
         $this->drawlabels();
+
+        $this->drawdatapoints();
     }
 
     private function definesafespace()
     {
         $this->safe_space = array(
-            "start_x" => $this->PADDING_X,
-            "end_x" => $this->image_width - $this->PADDING_X,
-            "start_y" => $this->image_height - $this->PADDING_Y,
-            "end_y" => $this->PADDING_Y
+            "start_x" => $this::PADDING_X,
+            "end_x" => $this->image_width - $this::PADDING_X,
+            "start_y" => $this->image_height - $this::PADDING_Y,
+            "end_y" => $this::PADDING_Y
         );
         $this->safe_image_width = $this->safe_space["end_x"] - $this->safe_space["start_x"];
         $this->safe_image_height = $this->safe_space["start_y"] - $this->safe_space["end_y"];
@@ -204,6 +206,46 @@ class Grapher
             }
         }
         imagesetstyle($this->image, $line_style_colors);
+    }
+
+    private function drawdatapoints()
+    {
+        foreach ($this->labels as $label)
+        {
+            $value = isset($this->data[$label]) ? $this->data[$label] : [];
+            if (count($value) > 0)
+            {
+                $this->drawdatapoint($label, $value[0]);
+            }
+        }
+    }
+
+    private function drawdatapoint($label, $value)
+    {
+        imageellipse(
+            $this->image,
+            $this->safe_space["start_x"] + $this->getlabelposition($label),
+            $this->safe_space["start_y"] - $this->getvalueposition($value),
+            $this::DOT_SIZE,
+            $this::DOT_SIZE,
+            $this->colors["red"]
+        );
+    }
+
+    private function getlabelposition($label)
+    {
+        $distance_between_labels = $this->safe_image_width / count($this->labels);
+        $label_position = array_search($label, $this->labels);
+        return $distance_between_labels * ($label_position + 1);
+    }
+
+    private function getvalueposition($value)
+    {
+        $upper_limit = doubleval(ceil(max(array_map("max", $this->data))) / 10 * 10);
+        $bottom_limit = doubleval(ceil(min(array_map("min", $this->data))) / 10 * 10);
+        $distance_between_labels = $this->safe_image_height / $this::Y_AXIS_LABEL_COUNT;
+        $step = ($upper_limit - $bottom_limit) / ($this::Y_AXIS_LABEL_COUNT - 1);
+        return $distance_between_labels * (abs($bottom_limit - $value) / $step + 1);
     }
 
     public function getimage()
